4079af641e2db19107f18614c3a087c94e3846ce
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-06-07 23:41:47 +0600

    create user with email and password implement done on AuthProvider

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -49,7 +49,7 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      console.log("", currentUser);
+      setLoading(false);
     });
     return () => {
       return unsubscribe();

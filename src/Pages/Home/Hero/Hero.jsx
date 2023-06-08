bb850be7f7d773ad872343e3d79f0f2c8ca2cbde
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-06-08 15:59:27 +0600

    make homepage responsive

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,25 +1,30 @@
 import { FaInfo } from "react-icons/fa";
 import { SiGoogleclassroom } from "react-icons/si";
 import { MdOutlineManageAccounts } from "react-icons/md";
+import heroImg from "../../../assets/teacher.png";
 const Hero = () => {
   return (
-    <div className="bg-gray-200 ">
-      <div className="max-w-screen-xl mx-auto grid items-center min-h-[80vh] lg:grid-cols-2 gap-6">
-        <div className=" space-y-6">
-          <h2 className="text-5xl text-gray-900 font-bold">
-            Get <span className=" text-orange-600">2500+</span> <br /> Best
-            Online Courses
-            <br /> From Polyglot Pioneers Academy
-          </h2>
-          <button className="flex items-center gap-2 bg-indigo-600 px-6 py-3 text-white rounded-md">
-            <FaInfo></FaInfo> Learn More
-          </button>
+    <>
+      <div className="py-3 px-8">
+        <div className="max-w-screen-xl mx-auto grid items-center min-h-[80vh] lg:grid-cols-2 gap-6">
+          <div className=" space-y-6">
+            <h2 className="text-3xl mt-4 lg:mt-0 lg:text-5xl text-gray-900 font-bold">
+              Get <span className=" text-orange-600">2500+</span> <br /> Best
+              Online Courses
+              <br /> From Polyglot Pioneers Academy
+            </h2>
+            <button className="flex items-center gap-2 bg-indigo-600 px-6 py-3 text-white rounded-md">
+              <FaInfo></FaInfo> Learn More
+            </button>
+          </div>
+          <div>
+            <img src={heroImg} />
+          </div>
         </div>
-        <div></div>
       </div>
       <div className=" bg-indigo-600 w-full">
-        <div className="max-w-screen-xl mx-auto grid grid-cols-2 lg:grid-cols-4 gap-6">
-          <div className="flex gap-3 items-center border-r-[1px] border-indigo-200 py-12">
+        <div className="max-w-screen-xl mx-auto grid md:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="flex gap-3 items-center border-b-[1px] lg:border-r-[1px] border-indigo-200 py-12">
             <div>
               <h2 className="text-4xl text-white bg-indigo-500 rounded-full p-4">
                 <SiGoogleclassroom />
@@ -32,7 +37,7 @@ const Hero = () => {
               </h2>
             </div>
           </div>
-          <div className="flex gap-3 items-center border-r-[1px] border-indigo-200 py-12">
+          <div className="flex gap-3 items-center border-b-[1px] lg:border-r-[1px] border-indigo-200 py-12">
             <div>
               <h2 className="text-4xl text-white bg-indigo-500 rounded-full p-4">
                 <MdOutlineManageAccounts />
@@ -45,7 +50,7 @@ const Hero = () => {
               </h2>
             </div>
           </div>
-          <div className="flex gap-3 items-center border-r-[1px] border-indigo-200 py-12">
+          <div className="flex gap-3 items-center border-b-[1px] md:border-0 lg:border-r-[1px] border-indigo-200 py-12">
             <div>
               <h2 className="text-4xl text-white bg-indigo-500 rounded-full p-4">
                 <SiGoogleclassroom />
@@ -73,7 +78,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 

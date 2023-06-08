1aec5ed50fd09d0ca3db2c49e331f70fa7264e3e
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-06-08 09:42:45 +0600

    added tanstack query and setup done

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,28 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // prime icons
 import "primeicons/primeicons.css";
 //theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
-    
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
 //core
-import "primereact/resources/primereact.min.css";                                       
+import "primereact/resources/primereact.min.css";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./Providers/AuthProvider";
-        
+
+// Create a client
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </HelmetProvider>
     </AuthProvider>
   </React.StrictMode>

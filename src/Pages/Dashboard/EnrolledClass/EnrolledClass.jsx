d7cdb896b3e5d87768c404774f42d2a4e55170f2
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-06-12 23:41:46 +0600

    got enrolled class data

--- src/Pages/Dashboard/EnrolledClass/EnrolledClass.jsx
+++ src/Pages/Dashboard/EnrolledClass/EnrolledClass.jsx
@@ -1,6 +1,19 @@
 import { Helmet } from "react-helmet-async";
-
+import useAuth from "../../../Hooks/useAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 const EnrolledClass = () => {
+  const { user, loading } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+  const { data: enrolledClass = [] } = useQuery({
+    queryKey: ["enrolled class"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/enrolledclass?email=${user?.email}`);
+      return res.data;
+    },
+    enabled: !loading && !!user?.email,
+  });
+  console.log(enrolledClass);
   return (
     <div>
       <Helmet>
